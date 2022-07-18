6fccc7b31690b54b486cf4c993e95348d20e0008
kensonjohnson <kenson.johnson@outlook.com>
2022-07-17 20:47:55 -0500

    Added color palette to CSS
    Link to palette is in README.md
    Started design of website.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # book_tracker
 
 This is a project from The Odin Project course material.
+
+Color palette used: https://colorhunt.co/palette/334257476072548ca8eeeeee
--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-function Book (title, author, numberOfPages, haveRead) {
-    this.title = title;
-    this.author = author;
-    this.numberOfPages = numberOfPages;
-    if (haveRead) {this.haveRead = "already read"} else {this.haveRead = "have not read"}
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.numberOfPages} pages, ${this.haveRead}.`
-    }
+let myLibrary = [];
+
+//Book object constructor
+function Book(title, author, numberOfPages, haveRead) {
+  this.title = title;
+  this.author = author;
+  this.numberOfPages = numberOfPages;
+  if (haveRead) {
+    this.haveRead = "already read";
+  } else {
+    this.haveRead = "have not read";
+  }
 }
 
-const theHobbit = new Book ('The Hobbit', 'J.R.R. Tolkien', 295, false);
+//set protype info function for Book objects
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.numberOfPages} pages, ${this.haveRead}.`;
+};
+
+function addBookToLibrary() {}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 
 console.log(theHobbit.info());
